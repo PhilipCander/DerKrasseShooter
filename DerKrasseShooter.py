221316a3298ff67bd4a0a4d5b083f5042cd4d5d2
PhilipCander <55022110+PhilipCander@users.noreply.github.com>
2019-11-19 00:32:07 +0100

    Add files via upload

--- DerKrasseShooter.py
+++ DerKrasseShooter.py
@@ -1,12 +1,25 @@
 from settings import *
-from scummbag import Block
+from classes import Block
+
 block_list = pygame.sprite.Group()
 block2_list = pygame.sprite.Group()
-
+missles = pygame.sprite.Group()
 all_sprites_list = pygame.sprite.Group()
+
 speed = 90
 wave = 3
 wave2 = 1
+cooldown = 100
+shoot = False
+
+
+def header():
+    pygame.draw.rect(window, black, (0, 0, screen_width, 75))
+    pygame.draw.rect(window, green, (((screen_width/2)-300), 50, 300, 20))
+
+
+def body():
+    pygame.draw.rect(window, black, (0, (screen_height-200), screen_width, 200))
 
 
 def blub():
@@ -18,7 +31,7 @@ def blub():
         block = Block(green, 50, 50)
 
         block.rect.x = random.randint(-300, -50)
-        block.rect.y = random.randrange(screen_height-50)
+        block.rect.y = random.randint(75, screen_height-250)
 
         block_list.add(block)
         all_sprites_list.add(block)
@@ -32,14 +45,14 @@ def blub2():
     for i in range(wave2):
         block2 = Block(red, 50, 50)
 
-        block2.rect.x = random.randint(-200, -50)
-        block2.rect.y = random.randrange(screen_height-50)
+        block2.rect.x = random.randint(-300, -50)
+        block2.rect.y = random.randrange(75, screen_height-250)
 
         block2_list.add(block2)
         all_sprites_list.add(block2)
 
-
-player = Block(red, 3, 3)
+missle = Block(white, 5, 10)
+player = Block(red, 25, 25)
 all_sprites_list.add(player)
 
 clock = pygame.time.Clock()
@@ -53,11 +66,19 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:
+                run = False
 
-    window.fill(black)
-
+    window.blit(bg, (0, 0))
+    #window.fill(white)
+    header()
+    body()
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+
+    for missle in missles:
+        missle.rect.y -= 10
 
     for block in block_list:
         block.rect.x += 3
@@ -66,13 +87,22 @@ while run:
         block2.rect.x += 5
 
     player.rect.x = mouse[0]
-    player.rect.y = mouse[1]
+    player.rect.y = 600
 
     if click[0] == 1:
-        blocks_hit_list = pygame.sprite.spritecollide(player, block_list, True)
-        blocks2_hit_list = pygame.sprite.spritecollide(player, block2_list, True)
+        missle = Block(white, 5, 10)
+        missle.rect.x = mouse[0]
+        missle.rect.y = 600
+        missles.add(missle)
+        all_sprites_list.add(missle)
 
-        print(block_list)
+    click = pygame.mouse.get_pressed()
+
+
+    blocks_hit_list = pygame.sprite.spritecollide(missle, block_list, True)
+    blocks2_hit_list = pygame.sprite.spritecollide(missle, block2_list, True)
+    if blocks_hit_list:
+        print("yeah")
 
     if count >= speed:
         blub()
--- settings.py
+++ settings.py
@@ -1,13 +1,19 @@
 import pygame
 import random
+import ctypes
+
+user32 = ctypes.windll.user32
+screen_width, screen_height = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+
 pygame.init()
 
-screen_width, screen_height = 800, 700
+#screen_width, screen_height = 1366, 768
 
-window = pygame.display.set_mode((screen_width, screen_height))
+window = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 
 red = (250, 0, 0)
 green = (0, 250, 250)
 black = (0, 0, 0)
+white = (250, 250, 250)
 
-
+bg = pygame.image.load("rec/bg.jpg")
